ffe1e76d27645be0dc439e3ac0f127dcce59a383
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-08-31 19:52:15 +0300

    adds bootstrap to the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import _ from 'lodash';
-import './style.css';
+import 'bootstrap';
+import './scss/style.scss';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  element.innerHTML = 'LeaderBoard';
+  element.classList.add('bg-info');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -19,9 +21,28 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins() { // post css plugins, can be exported to postcss.config.js
+              return [
+                precss,
+                autoprefixer,
+              ];
+            },
+          },
+        }, {
+          loader: 'sass-loader', // compiles Sass to CSS
+        }],
       },
     ],
+  },
+  performance: {
+    hints: false,
   },
 };
