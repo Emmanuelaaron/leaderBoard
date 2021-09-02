59263433bdecca061105d7b503c6c0cef4066ba7
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-09-02 11:41:03 +0300

    modifies functions to use fetch instead of xmlHTTPrequest

--- src/request.js
+++ src/request.js
@@ -1,28 +1,36 @@
+import display from "./bodyComponent";
+
 const request = new XMLHttpRequest();
 const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/9GFpJH58W0GP2nThYOqo/scores';
 
-async function getScores() {
-  const promise = new Promise((res) => {
-    request.open('GET', requestURL);
-    request.onload = () => {
-      if (request.status === 200) {
-        res(request.response);
-      } else {
-        res('Error');
-      }
-    };
-    request.send();
-  });
-  const list = JSON.parse(await promise);
 
-  return list;
+async function getScores(){
+  const response = await fetch(requestURL)
+  const list = await response.json()
+  console.log("ftech console",list)
+  return list
 }
 
 const scoreAddition = (nameInput, scoreInput) => {
-  const params = `user=${nameInput}&score=${scoreInput}`;
-  request.open('POST', requestURL, true);
-  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  request.send(params);
-};
+  let postData = {
+    "user": nameInput,
+    "score": scoreInput
+  }
+  fetch(requestURL, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }, 
+    body: JSON.stringify(postData)
+  })
+  .then((response) => response.json())
+  .then((message) => {
+    if(message.result === "Leaderboard score created correctly."){
+      document.body.innerHTML = ''
+      display()
+    }
 
+  })
+  
+}
 export { getScores, scoreAddition };
